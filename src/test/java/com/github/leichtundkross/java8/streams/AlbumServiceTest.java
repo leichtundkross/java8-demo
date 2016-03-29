878e06957219a6b334c0d49b7ec310c7be3c957a
Andreas Guenzel <andreas.guenzel@exxeta.com>
2016-03-29 19:25:07 +0200

    streams

---
+++ src/test/java/com/github/leichtundkross/java8/streams/AlbumServiceTest.java
@@ -0,0 +1,47 @@
+package com.github.leichtundkross.java8.streams;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AlbumServiceTest {
+
+	private AlbumService service = new AlbumService();
+
+	@Test
+	public void findAlbumNamesHavingTracksRatedFourOrHigherSortedByName() {
+		Collection<Album> albumsToSearch = createAlbums();
+
+		List<Album> favorites = service.findAlbumNamesHavingTracksRatedFourOrHigherSortedByName(albumsToSearch);
+
+		assertEquals(2, favorites.size());
+		assertEquals("album2", favorites.get(0).getName());
+		assertEquals("album4", favorites.get(1).getName());
+	}
+
+	@Test
+	public void findAlbumNamesHavingTracksRatedFourOrHigherSortedByNameWithJava8() {
+		Collection<Album> albumsToSearch = createAlbums();
+
+		List<Album> favorites = service.findAlbumNamesHavingTracksRatedFourOrHigherSortedByNameWithJava8(albumsToSearch);
+
+		assertEquals(2, favorites.size());
+		assertEquals("album2", favorites.get(0).getName());
+		assertEquals("album4", favorites.get(1).getName());
+	}
+
+	private static Collection<Album> createAlbums() {
+		Collection<Album> albumsToSearch = Arrays.asList(
+				new Album("album1").addTrack(new Track(1)).addTrack(new Track(1)).addTrack(new Track(3)).addTrack(new Track(2)), //
+				new Album("album4").addTrack(new Track(1)).addTrack(new Track(1)).addTrack(new Track(10)), //
+				new Album("album3").addTrack(new Track(0)).addTrack(new Track(2)).addTrack(new Track(2)), //
+				new Album("album2").addTrack(new Track(1)).addTrack(new Track(6)) //
+		);
+
+		return albumsToSearch;
+	}
+}
