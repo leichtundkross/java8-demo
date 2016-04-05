bff1265ff392ebe84dde7f07b45cd23b0083575a
Andreas Guenzel <andreas.guenzel@exxeta.com>
2016-04-05 14:34:55 +0200

    typed annotations

---
+++ src/test/java/com/github/leichtundkross/java8/typeannotations/AnnotateNonNullTest.java
@@ -0,0 +1,17 @@
+package com.github.leichtundkross.java8.typeannotations;
+
+import org.junit.Test;
+
+public class AnnotateNonNullTest {
+
+	@Test
+	public void acceptAnyButNotNull() {
+		new AnnotateNonNull().acceptAnyButNotNull("hello");
+	}
+
+	@Test
+	public void acceptAnyButNotNull_NPE() {
+		// this issue should be found using findbugs
+		new AnnotateNonNull().acceptAnyButNotNull(null);
+	}
+}
