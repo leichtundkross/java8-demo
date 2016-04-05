f92d8a889250d97783ce0e7c4756775e78a542b6
leichtundkross <a.guenzel@andig.de>
2016-04-05 11:36:14 +0200

    static methods in interfaces

--- src/main/java/com/github/leichtundkross/java8/defaultmethods/Interface1.java
+++ src/main/java/com/github/leichtundkross/java8/defaultmethods/Interface1.java
@@ -2,6 +2,9 @@ package com.github.leichtundkross.java8.defaultmethods;
 
 public interface Interface1 {
 
+	/**
+	 * Default method needs keyword default
+	 */
 	default String sayHello() {
 		return "Interface1: Hello";
 	}
--- src/main/java/com/github/leichtundkross/java8/defaultmethods/Interface2.java
+++ src/main/java/com/github/leichtundkross/java8/defaultmethods/Interface2.java
@@ -2,6 +2,9 @@ package com.github.leichtundkross.java8.defaultmethods;
 
 public interface Interface2 {
 
+	/**
+	 * Default method needs keyword default
+	 */
 	default String sayHello() {
 		return "Interface2: Hello";
 	}
