970d2aee560e365c9d28ded5723a009251752a8e
leichtundkross <a.guenzel@andig.de>
2016-04-08 08:18:51 +0200

    java 4 generics

--- src/main/java/com/github/leichtundkross/java8/typeinference/GenericEvolution.java
+++ src/main/java/com/github/leichtundkross/java8/typeinference/GenericEvolution.java
@@ -4,13 +4,26 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class GenericyEvolution {
+public class GenericEvolution {
+
+	/**
+	 * We didn't have any generics at all :-/
+	 */
+	public void java4Style() {
+		Set minions = new TreeSet();
+		minions.add("Bob");
+		minions.add("Kevin");
+		minions.add("Stuart");
+		minions.addAll(Arrays.asList(new String[] { "Fred" }));
+		minions.addAll(Arrays.asList(new String[] {}));
+	}
 
 	public void java5Style() {
 		Set<String> minions = new TreeSet<String>();
 		minions.add("Bob");
 		minions.add("Kevin");
 		minions.add("Stuart");
+		minions.addAll(Arrays.asList("Fred"));
 		minions.addAll(Arrays.<String> asList());
 	}
 
@@ -19,6 +32,7 @@ public class GenericyEvolution {
 		minions.add("Bob");
 		minions.add("Kevin");
 		minions.add("Stuart");
+		minions.addAll(Arrays.asList("Fred"));
 		minions.addAll(Arrays.<String> asList());
 	}
 
@@ -27,6 +41,7 @@ public class GenericyEvolution {
 		minions.add("Bob");
 		minions.add("Kevin");
 		minions.add("Stuart");
+		minions.addAll(Arrays.asList("Fred"));
 		minions.addAll(Arrays.asList());
 	}
 }
